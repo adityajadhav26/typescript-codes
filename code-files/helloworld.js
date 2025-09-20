88aa2e95ce31b109d8eb3010c8a229f5a042af2e
Aditya Jadhav <adityabjadhav8@gmail.com>
2025-09-20 15:18:16 +0530

    add helloworld code

---
+++ code-files/helloworld.js
@@ -0,0 +1,9 @@
+console.log("hello world");
+console.log("Welcome to typescript");
+// single line comment      macos - command+/       windows os- ctrl+A
+/* this
+is
+multi
+line
+comment  shift+optipn+A     shift+ alt + A
+*/ 
