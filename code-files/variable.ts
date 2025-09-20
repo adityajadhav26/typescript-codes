d0e62bb600886b52272261385b3eadc463a627c7
Aditya Jadhav <adityabjadhav8@gmail.com>
2025-09-20 16:18:59 +0530

    Add variables demo

---
+++ code-files/variable.ts
@@ -0,0 +1,15 @@
+// we dont use var in modern days of JS/TS
+
+let x; // declaration
+x =10; // assignment/ iniatialization
+console.log(x);     //10
+x=20;   //re assignment
+console.log(x);     // 20
+
+
+const pi=3.14;  // must initilized at declaration, 
+console.log(pi);
+
+// both let, const are block scope.
+// both dont support Hoisting.
+
